c9688a66ab16b379b93ed8b3183ddfbbbed0bb70
Linda LW <lindalw03@gmail.com>
2021-12-05 16:52:33 -0500

    still trying to figure out delete

--- server.js
+++ server.js
@@ -88,52 +88,15 @@ app.post('/api/notes', (req, res) => {
 	}
 });
 
-// GET Route for retrieving all the feedback
-// app.get('/api/feedback', (req, res) => {
-//   console.info(`${req.method} request received for feedback`);
-
-//   readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
-// });
-
-// POST Route for submitting feedback
-// app.post('/api/feedback', (req, res) => {
-//   // Log that a POST request was received
-//   console.info(`${req.method} request received to submit feedback`);
-
-//   // Destructuring assignment for the items in req.body
-//   const { email, feedbackType, feedback } = req.body;
-
-//   // If all the required properties are present
-//   if (email && feedbackType && feedback) {
-//     // Variable for the object we will save
-//     const newFeedback = {
-//       email,
-//       feedbackType,
-//       feedback,
-//       feedback_id: uuid(),
-//     };
-
-//     readAndAppend(newFeedback, './db/feedback.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newFeedback,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting feedback');
-//   }
-// });
 
 app.delete('/api/notes/:id', (req, res) => {
-	console.info(`${req.method} request received for notes id`, JSON.parse(req));
+	console.info(`${req.method} request received for notes id`);
 });
 
 // GET Route for homepage
 app.get('*', (req, res) => {
 	console.log('in wildcard');
-	console.info(`${req.method} request received for wildcard`, `${req}`);
+	console.info(`${req.method} request received for wildcard`);
 	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
